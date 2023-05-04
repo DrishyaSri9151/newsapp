ea0b41ffd644657626183f1f9d307c04a5303b34
drishyasrivastava <srivastavadrishya1@gmail.com>
2023-05-04 10:28:40 +0530

    infinite scroller

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,78 +1,116 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
-import Spinner from './Spinner';
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 8,
+    category: "general",
+  };
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
 
-    constructor(){
-        super();
-        console.log("hello i am a constructor from news component");
-        this.state={
-            articles: [],
-            loading: false,
-            page:1
-        }
-    }
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
-    async componentDidMount(){
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4c9fa14a51574076ae514d4c4d75f734&page=1&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true})
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({articles: parsedData.articles ,  
-            totalResults : parsedData.totalResults,
-            loading:false
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: true,
+      page: 1,
+      totalResults: 0,
+    };
+    document.title = `${this.capitalizeFirstLetter(
+      this.props.category
+    )}-NewsEra`;
+  }
 
-    handlePrevClick = async () => {
-        console.log("previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4c9fa14a51574076ae514d4c4d75f734&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-        let data = await fetch(url);
-        this.setState({loading:true})
-        let parsedData = await data.json();
-        this.setState({
-            page : this.state.page-1,
-            articles: parsedData.articles,
-            loading:false
-        })
-    }
-    handleNextClick = async () => {
-        console.log("Next");
-        if(!(this.state.page+1 > (Math.ceil(this.state.totalResults/this.props.pageSize)))){
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4c9fa14a51574076ae514d4c4d75f734&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true})
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({
-            page : this.state.page+1,
-            articles: parsedData.articles,
-            loading:false
-        })
-    }
-    }
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4c9fa14a51574076ae514d4c4d75f734&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  }
 
+  async componentDidMount() {
+    this.updateNews();
+  }
 
+  handlePrevClick = async () => {
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
+  };
+  handleNextClick = async () => {
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
+  };
+
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4c9fa14a51574076ae514d4c4d75f734&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+    });
+  };
 
   render() {
     return (
-      <div className="container my-3">
-        <h1 className="text-center"> NewsEra - Top Headlines</h1>
-        {this.state.loading && <Spinner/>}
-        <div className="row">
-            {!this.state.loading && this.state.articles.map((element)=>{
-               return <div className="col-md-4" key={element.url}>
-               <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage?element.urlToImage:"https://w7.pngwing.com/pngs/982/544/png-transparent-news-graphy-logo-icon-news-logo-text-photography-computer-wallpaper.png "} newsUrl={element.url}/>
-               </div>
-            })}
-        </div>
-        <div className="container d-flex justify-content-between" >
-        <button disabled={this.state.page <=1} type="button" class="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous {this.state.page-1}</button>
-        <button disabled={this.state.page+1 > (Math.ceil(this.state.totalResults/this.props.pageSize))} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr; {this.state.page}</button>
-        </div>
-      </div>
-    )
+      <>
+        <h1 className="text-center" style={{margin: '35px 0px' }}>
+          NewsEra - Top {this.props.category} Headlines
+        </h1>
+        {this.state.loading && <Spinner />}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      description={
+                        element.description ? element.description : ""
+                      }
+                      imageUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://w7.pngwing.com/pngs/982/544/png-transparent-news-graphy-logo-icon-news-logo-text-photography-computer-wallpaper.png "
+                      }
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
+    );
   }
 }
 
-export default News
+export default News;
